8762682785e3d5a9285ff5151046bcaefbe14dcc
h4hspace <166570671+h4hspace@users.noreply.github.com>
2024-04-10 00:25:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,56 @@
     let timerId;
 
     const questions = [
-        // Your questions here
+       {
+    question: "Where were the apostles fishing when they couldnt catch fish all night?",
+    choices: ["Sea of Sodom", "Sea of Galilee", "Sea of Helath", "Mediterranean Sea"],
+    correctAnswer: "Sea of Galilee"
+},
+{
+    question: "How many days later did Jesus come to see Lazarus afte he died?",
+    choices: ["Three Days", "Five Days", "Two Days", "Four Days"],
+    correctAnswer: "Four Days"
+},
+{
+    question: "What was the king's name in Esther's story?",
+    choices: ["Nebucadnezar", "Ahab", "Xerxes", "Josiah"],
+    correctAnswer: "Xerxes"
+},
+{
+    question: "How many days did Esther pray for before going to see the king?",
+    choices: ["7 days", "2 days", "4 days", "3 days"],
+    correctAnswer: "3 days"
+},
+{
+    question: "In what town did Zacchaeus live?",
+    choices: ["Damascus", "Jericho", "Capernaum", "Bethlehem"],
+    correctAnswer: "Jericho"
+},
+{
+    question: "How many times more did Zacchaeus agree to pay back to those he cheated out on tax?",
+    choices: ["2 times more", "5 times more", "4 times more", "3 times more"],
+    correctAnswer: "4 times more"
+},
+{
+    question: "What was the montain name where Abraham went to sacrifice Isaac?",
+    choices: ["Mount Zion", "Mount Moriah", "Mount Carmel", "Mount Tabor"],
+    correctAnswer: "Mount Moriah"
+},
+{
+    question: "What town was the paralytic man from?",
+    choices: ["Capernaum", "Damascus", "Bethsaida", "Hebron"],
+    correctAnswer: "Capernaum"
+},
+{
+    question: "What was the name of the king that ruled during Daniel times in the Medo-Persian Empire?",
+    choices: ["Hezekiah", "Ahab", "Darius", "Nebucadnezar"],
+    correctAnswer: "Darius"
+},
+{
+    question: "What sea did Jesus cross before the 2 fish and 5 breads miracle?",
+    choices: ["Sea of Sodom", "Sea of Tiberias", "Sea of Galilee", "Mediteranean Sea"],
+    correctAnswer: "Sea of Galilee"
+}
     ];
 
     submitNameButton.addEventListener('click', () => {
