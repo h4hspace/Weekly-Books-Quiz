fd2953a245327b47907e4d887593a0f291511a33
h4hspace <166570671+h4hspace@users.noreply.github.com>
2024-04-18 08:31:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,54 +17,54 @@
 
     const questions = [
        {
-    question: "Where were the apostles fishing when they couldn't catch fish all night?",
-    choices: ["Sea of Sodom", "Sea of Galilee", "Sea of Helath", "Mediterranean Sea"],
-    correctAnswer: "Sea of Galilee"
+    question: "What does the word Israel mean?",
+    choices: ["My People", "The choesen", "Prevails with God", "Stays with God"],
+    correctAnswer: "Prevails with God"
 },
 {
-    question: "How many days later did Jesus come to see Lazarus after he died?",
-    choices: ["Three Days", "Five Days", "Two Days", "Four Days"],
-    correctAnswer: "Four Days"
+    question: "Who was the only female judge in Israel?",
+    choices: ["Deborah", "Naomi", "Rachael", "Elisheba"],
+    correctAnswer: "Deborah"
 },
 {
-    question: "What was the king's name in Esther's story?",
-    choices: ["Nebucadnezar", "Ahab", "Xerxes", "Josiah"],
-    correctAnswer: "Xerxes"
+    question: "Who is considered thw 13th apostle to take Judah's place?",
+    choices: ["Josiah", "Luke", "Matthias", "James"],
+    correctAnswer: "Matthias"
 },
 {
-    question: "How many days did Esther pray for before going to see the king?",
-    choices: ["7 days", "2 days", "4 days", "3 days"],
-    correctAnswer: "3 days"
+    question: "What is the name of the first 5 books of the bible?",
+    choices: ["The Torah", "The Pentateuch", "The Law", "The Midrah"],
+    correctAnswer: "The Pentateuch"
 },
 {
-    question: "In what town did Zacchaeus live?",
-    choices: ["Damascus", "Jericho", "Capernaum", "Bethlehem"],
-    correctAnswer: "Jericho"
+    question: "Which of the following biblical figures does NOT have a recorded bible dream?",
+    choices: ["Pilate's wife", "Solomon", "Mary", "Joseph"],
+    correctAnswer: "Pilate's wife"
 },
 {
-    question: "How many times more did Zacchaeus agree to pay back to those he cheated out on taxes?",
-    choices: ["2 times more", "5 times more", "4 times more", "3 times more"],
-    correctAnswer: "4 times more"
+    question: "What is the shortest book of the Bible",
+    choices: ["Jude", "Philemon", "3 John", "Obadiah"],
+    correctAnswer: "Jude"
 },
 {
-    question: "What was the montain name where Abraham went to sacrifice Isaac?",
-    choices: ["Mount Zion", "Mount Moriah", "Mount Carmel", "Mount Tabor"],
-    correctAnswer: "Mount Moriah"
+    question: "How many languages was the bible written in?",
+    choices: ["2", "3", "1", "4"],
+    correctAnswer: "3"
 },
 {
-    question: "What town was the paralytic man from?",
-    choices: ["Capernaum", "Damascus", "Bethsaida", "Hebron"],
-    correctAnswer: "Capernaum"
+    question: "Which prophet did King Hezekiah consult when Sennacharib threatened Jerusalem?",
+    choices: ["Zechariah", "Micah", "Isaiah", "Elijah"],
+    correctAnswer: "Isaiah"
 },
 {
-    question: "What was the name of the king that ruled during Daniel times in the Medo-Persian Empire?",
-    choices: ["Hezekiah", "Ahab", "Darius", "Nebucadnezar"],
-    correctAnswer: "Darius"
+    question: "In which book of the Bible will you find the story of the Tower of Babel?",
+    choices: ["Genesis", "Exodus", "Deuteronomy", "Numbers"],
+    correctAnswer: "Genesis"
 },
 {
-    question: "What sea did Jesus cross before the 2 fish and 5 breads miracle?",
-    choices: ["Sea of Sodom", "Sea of Tiberias", "Sea of Galilee", "Mediteranean Sea"],
-    correctAnswer: "Sea of Galilee"
+    question: "Which prophet was taken to Egypt against his will after the fall of Jerusalem?",
+    choices: ["Joseph", "Daniel", "Jeremiah", "Malechi"],
+    correctAnswer: "Jeremiah"
 }
     ];
 
